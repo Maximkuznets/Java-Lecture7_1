2a828f49e51d269c944124f7f9f26f7da3c05033
Кузнецов Максим <maximkuznetsov17250@gmail.com>
2021-10-10 20:23:38 +0300

    added Cheсk 100 % COVEREDRATIO in INSTRUCTION

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -9,8 +9,8 @@ class StatisticServiceTest {
     void findMax() {
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12,13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
